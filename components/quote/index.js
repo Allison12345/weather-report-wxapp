60952e4978910602521880576d602fd97923a867
Allison12345 <2562392373@qq.com>
2020-03-21 22:07:34 +0800

    quote

---
+++ components/quote/index.js
@@ -0,0 +1,26 @@
+Component({
+  data: {
+    content: ''
+  },
+  lifetimes: {
+    attached: function() {
+      this.getQuote()
+    }
+  },
+  methods: {
+    getQuote() {
+      wx.request({
+        url: 'http://api.tianapi.com/txapi/dujitang/index',
+        data: {
+          key: '581c3e8dc9c3a7c8bd070d2ba13ea450'
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        success: res => {
+          this.setData({ content: res.data.newslist[0].content })
+        }
+      })
+    }
+  }
+})
