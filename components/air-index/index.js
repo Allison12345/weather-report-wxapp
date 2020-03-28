cd2cee72d8230198368968a5263e5a6a1952ddd8
Allison <2562392373@qq.com>
2020-03-28 15:53:57 +0800

    style fixed

--- components/air-index/index.js
+++ components/air-index/index.js
@@ -1,28 +1,21 @@
-const airDict = require('./config')
-//const computedBehavior = require('../../node_modules/miniprogram-computed/miniprogram_dist/index.js')
+const airDict = require("./config");
 Component({
-  //behaviors: [computedBehavior],
   properties: {
     air: String,
     air_level: String
   },
-  data:{
-    color:'1'
+  data: {
+    color: ""
   },
   lifetimes: {
     attached() {
-      this.setData({color: airDict[this.properties.air_level] || 'green'})
-      console.log(this.data.color,this.properties.air_level)
+      this.setData({ color: airDict[this.properties.air_level] || "green" });
+      console.log(this.data.color, this.properties.air_level);
     }
   },
-  computed: {
-    cptColor() {
-      return airDict[this.properties.air_level] || 'green'
-    }
-  },
-  methods:{
-    onInfoShow(){
-      this.triggerEvent("onInfoShow")
+  methods: {
+    onInfoShow() {
+      this.triggerEvent("onInfoShow");
     }
   }
-})
+});
