b7d840efee00b46ffe352c1cdf2df4b0c5680c50
Allison <2562392373@qq.com>
2020-03-29 22:14:23 +0800

    my-weather-search

--- components/my-weather-search/index.js
+++ components/my-weather-search/index.js
@@ -1,6 +1,39 @@
 Component({
-  data:{
-    img:'https://6d79-mywxapp-q4z0b-1301425530.tcb.qcloud.la/search-outline.png?sign=5fd5c85d0e4b46858a35d07b2726fcde&t=1584787271',
-    city:''
+  data: {
+    img:
+      'https://6d79-mywxapp-q4z0b-1301425530.tcb.qcloud.la/search-outline.png?sign=5fd5c85d0e4b46858a35d07b2726fcde&t=1584787271',
+    city: ''
+  },
+  methods: {
+    onComfirm(e) {
+      this.getCityInfo(e.detail.value)
+    },
+    getCityInfo(address) {
+      wx.request({
+        url: `https://apis.map.qq.com/ws/geocoder/v1/?address=${address}`,
+        data: {
+          key: 'QGIBZ-PBXKV-DLQPX-UXX53-RYIS6-FCF2W'
+        },
+        header: {
+          'content-type': 'application/json'
+        },
+        success: res => {
+          const { status, message, result } = res.data
+          if (status === 0) {
+            const { province, city, district } = result.address_components
+            wx.navigateTo({
+              url: `/pages/weather-page/index?city=${province +
+                city +
+                district}`
+            })
+            return
+          }
+          wx.showToast({ title: message, icon: 'none' })
+        },
+        fail: res => {
+          wx.showToast({ title: res.message, icon: 'none' })
+        }
+      })
+    }
   }
 })
